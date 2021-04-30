c7c8a78e8d0c5a99e2d1cb72541f7bf7e5901e5a
Navilongo <navilongo@gmail.com>
2021-04-29 21:05:16 -0700

    merging both dataframes on jupyter and joining on sql

--- etl_challenge.sql
+++ etl_challenge.sql
@@ -1,13 +1,25 @@
+drop table zillow_per_sqr
 create table zillow_per_sqr (
 	id serial Primary Key,
-	zillow_date timestamp,
+	dates Date,
 	price_per_sqr float
 	);
-	
+
+drop table gas_prices
 create table gas_prices (
 	id serial primary key,
+	
 	reg_gas_price float,
 	midgrade_gas_price float,
 	premium_gas_price float,
-	gas_date timestamp
+	dates Date
 );
+
+select * from gas_prices;
+
+select * from zillow_per_sqr;
+
+select z.dates, z.price_per_sqr, g.reg_gas_price, g.midgrade_gas_price, g.premium_gas_price
+FROM zillow_per_sqr as z
+JOIN gas_prices as g 
+ON z.dates = g.dates;
